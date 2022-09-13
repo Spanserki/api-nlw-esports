9b91ccf75df3ee425a6b137b9ca6a3820a275a19
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-09-13 11:07:54 -0300

    Configuring TypeScript in the API

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000);
+
+app.get('/ads', (req, res) => {
+    return res.json([
+        
+        {"id": 1, "name": "Guilherme", "idade": 23},
+        {"id": 1, "name": "Guilherme", "idade": 23},
+        {"id": 1, "name": "Guilherme", "idade": 23},
+        {"id": 1, "name": "Guilherme", "idade": 23},
+
+    ])
+})
