c9ae85111ec62ee9df8becd38f792f3cf7b7a0b5
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-09-15 20:04:36 -0300

    Prism and SqLite database configured

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,30 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
-app.listen(3000);
+const prisma = new PrismaClient()//Conexão com o banco
 
 app.get('/ads', (req, res) => {
+    return res.json([])
+})
+
+app.post('/ads', (req, res) => {
+    return res.status(201).json([])
+})
+
+app.get('/games', (req, res) => {
+
     return res.json([
         
         {"id": 1, "name": "Guilherme", "idade": 23},
-        {"id": 1, "name": "Guilherme", "idade": 23},
-        {"id": 1, "name": "Guilherme", "idade": 23},
-        {"id": 1, "name": "Guilherme", "idade": 23},
-
+        {"id": 2, "name": "Guilherme", "idade": 23},
+        {"id": 3, "name": "Guilherme", "idade": 23},
+        {"id": 4, "name": "Guilherme", "idade": 23},
     ])
 })
+
+app.get('/ads/:id/discord', (req, res) => {
+    return res.json([])
+})
+
+app.listen(3000);
